09a39024a0708aa3a9a45d31be5f295efc664376
BremK <mohammed1998-kurder@hotmail.com>
2024-04-11 18:42:12 +0200

    added command function

--- bot.js
+++ bot.js
@@ -3,39 +3,72 @@
 */
 
 require('dotenv').config();
-const { Client, GatewayIntentBits, Partials, Collection, ActivityType } = require('discord.js')
-
+const { Client, GatewayIntentBits, Partials } = require("discord.js")
+const axios = require("axios");
+const prefix = process.env.PREFIX
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent
+        GatewayIntentBits.MessageContent,
     ],
-    partials: [Partials.Channel, Partials.Message, Partials.User, Partials.GuildMember, Partials.Reaction]
+    partials: [
+        Partials.Channel,
+        Partials.Message,
+        Partials.User,
+        Partials.GuildMember,
+        Partials.Reaction,
+    ],
 })
-const tokens ="MTIyNzkxNzg2ODE0MjY5MDMzNQ.Gb6td8.E5Yrp98oOrsRlApkGzW-dXDr3Qu0x5pcFfcj2E"
+
+const tokens = process.env.TOKEN
 
 console.log(tokens)
 // The ready event is vital, it means that your bot will only start reacting to information
 // from Discord after ready is emitted
 
-// Log our bot in
+// counting amount of images
+let counter= 0
 
+//https://api.imgflip.com/get_memes --> get
 
-client.on('ready', () => {
-    console.log('I am ready!');
-});
+ client.on('messageCreate', async msg => {
+    // not start with prefix -> return early
+    if (!msg.content.startsWith(prefix)) return;
 
-// Create an event listener for messages
-client.on('message', message => {
-    // If the message is "ping"
-    console.log("step 1")
-    if (message.content === 'ping') {
-        console.log("step 2")
-        // Send "pong" in the same channel
-        message.channel.send('pong');
+    // remove prefix to get command name
+    const commandName = msg.content.slice(prefix.length);
+
+    // check for command name directly
+    if (commandName === 'meme') {
+        const response = await axios.get('https://api.imgflip.com/get_memes')
+        const randomNumber = Math.floor(Math.random() * response.data.data.memes.length);
+        const meme =  response.data.data.memes[randomNumber].url
+            counter++
+
+        msg.channel.send(` ${msg.author} requested a meme! :smirk: → ` + meme + "  Amount of memes: " +  counter)
     }
-    console.log("step 3")
-});
 
-client.login(tokens);
+    else{
+        return
+    }
+})
+
+
+client.on("ready", () => {
+    console.log("I am ready!")
+})
+
+function execute(message, args) {
+    // Check if arguments are provided
+    if (!args.length) {
+        return message.channel.send('You need to provide some text to repeat!');
+    }
+
+    // Join the arguments into a single string
+    const text = args.join(' ');
+
+    // Send the repeated text to the channel
+    message.channel.send(text);
+}
+client.login(tokens)
